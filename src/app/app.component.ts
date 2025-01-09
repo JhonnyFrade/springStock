bf901cb383b05daa1e08f4f776010a311e8d9c92
JhonnyFrade <fradeww@gmail.com>
2025-01-09 10:25:16 -0300

    feat: criação do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'springStock';
+
+  title = 'SpringStock'
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Config', action: this.onClick.bind(this) }
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item');
+  }
 }
